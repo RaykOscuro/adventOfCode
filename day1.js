48f77b7df06eb25c9aea451565e7d1f488c80815
Rayk Oscuro <wer.auch.immer@gmx.net>
2023-12-13 03:51:50 +0100

    First 3 days of the Advent of Code done! \o/

---
+++ day1.js
@@ -0,0 +1,59 @@
+import { readFileSync } from "fs";
+import { posix } from "path";
+
+const file = readFileSync("./inputDataDay1.txt", "utf-8");
+
+console.log(typeof file.split("\n"));
+
+const digits = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const writtenDigits = [
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+];
+
+let sumOfCoords = 0;
+
+for (let x of file.split("\n")) {
+  console.log("new one!");
+  let firstDigit = null;
+  let secondDigit = null;
+  console.log(x);
+  for (let pos = 0; pos < 9; pos++) {
+ //   while (x.indexOf(writtenDigits[pos]) > -1) {
+        x=x.replace(writtenDigits[pos],(writtenDigits[pos][0]+digits[pos]+writtenDigits[pos].slice(-1)));
+ //   }
+  }
+  console.log(x);
+  for (let y = 0; y < x.length; y++) {
+    if (digits.indexOf(x[y]) > -1) {
+      console.log(`found ${x[y]}`);
+      firstDigit = x[y];
+      break;
+    }
+  }
+  for (let z = x.length - 1; z > -1; z--) {
+    if (digits.indexOf(x[z]) > -1) {
+      console.log(`found ${x[z]}`);
+      secondDigit = x[z];
+      break;
+    }
+  }
+  const coords = Number(firstDigit + secondDigit);
+  console.log(coords);
+  sumOfCoords += coords;
+  console.log(`finalCoords: ${sumOfCoords}`);
+}
+
+let x="twonesixthreeight";
+console.log(x);
+for (let pos = 0; pos < 9; pos++) {
+      x=x.replace(writtenDigits[pos],(writtenDigits[pos][0]+digits[pos]+writtenDigits[pos].slice(-1)));
+  }
+console.log(x);
---
+++ day2.js
@@ -0,0 +1,56 @@
+import { readFileSync } from "fs";
+import { posix } from "path";
+
+const file = readFileSync("./inputDataDay2.txt", "utf-8");
+
+const splitFile = file.split("\r\n");
+
+function extractID(input) {
+  return input.split(":")[0].slice(5);
+}
+
+function extractGames(input) {
+  return input.split(":")[1].split(";");
+}
+
+function getAmount(game, index) {
+  let myAmount = 0;
+  let x = 0;
+  while (!isNaN(Number(game[index - x - 2]))) {
+    myAmount += game[index - x - 2] * Math.pow(10, x);
+    x++;
+  }
+  return myAmount;
+}
+
+var sumOfIDs = 0;
+var powerSet = 0;
+for (let x of file.split("\r\n")) {
+  var currentGames = extractGames(x);
+  var maxRed = 0;
+  var maxGreen = 0;
+  var maxBlue = 0;
+  for (const game of currentGames) {
+    const redIndex = game.indexOf("red");
+    const greenIndex = game.indexOf("green");
+    const blueIndex = game.indexOf("blue");
+    maxRed = Math.max(maxRed, getAmount(game, redIndex));
+    maxGreen = Math.max(maxGreen, getAmount(game, greenIndex));
+    maxBlue = Math.max(maxBlue, getAmount(game, blueIndex));
+  }
+  //  console.log(currentGames,maxRed,maxGreen,maxBlue)
+  // if (maxRed<13 && maxGreen<14 && maxBlue<15){
+  //   sumOfIDs+=Number(extractID(x));
+  //   console.log(extractID(x));
+  // }
+
+  powerSet = maxRed * maxGreen * maxBlue;
+  sumOfIDs += powerSet;
+}
+
+console.log(sumOfIDs);
+
+// console.log(isNaN(Number(currentGames[0][3])));
+
+// const blueIndex = extractGames(splitFile[0])[0].indexOf("blue");
+// const greenIndex = extractGames(splitFile[0])[0].indexOf("green");
