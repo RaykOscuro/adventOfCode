faf697a067cd17479cb12bb6bb8872f2284b38f8
Rayk Oscuro <wer.auch.immer@gmx.net>
2023-12-23 05:19:07 +0100

    solved day 9! <3

---
+++ day9_part1.js
@@ -0,0 +1,50 @@
+import { readFileSync } from "fs";
+import { get } from "http";
+import { posix } from "path";
+
+const file = readFileSync("./inputDataDay9.txt", "utf-8");
+const splitFile = file.split("\r\n");
+for (let x = 0; x < splitFile.length; x++) {
+  splitFile[x] = splitFile[x].split(" ");
+  for (let y = 0; y < splitFile[x].length; y++) {
+    splitFile[x][y] = Number(splitFile[x][y]);
+  }
+}
+
+function getDiffArray(inputArray) {
+  const diffArray = [];
+  for (let x = 0; x < inputArray.length - 1; x++) {
+    diffArray.push(inputArray[x + 1] - inputArray[x]);
+  }
+  return diffArray;
+}
+
+function noDiffs(inputArray) {
+  // if (inputArray.isEmpty()){
+  //   return false;
+  // }
+  for (const x of inputArray) {
+    if (x !== 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+let addedResults = 0;
+
+for (const data of splitFile) {
+  const diffArray = [data];
+  let helperArray = [];
+  //console.log(getDiffArray(diffArray[diffArray.length-1]))
+  while (!noDiffs(diffArray[diffArray.length-1])){
+     diffArray.push(getDiffArray(diffArray[diffArray.length-1]))
+  }
+  console.log(diffArray)
+  for (let x=diffArray.length-2; x>=0; x--){
+    diffArray[x].push(diffArray[x][diffArray[x].length-1]+diffArray[x+1][diffArray[x+1].length-1])
+  }
+  addedResults+=diffArray[0][diffArray[0].length-1];
+}
+
+console.log(addedResults);
