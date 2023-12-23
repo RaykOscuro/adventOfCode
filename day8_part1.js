76477732bdc137a544b2314a082b6378d445a53b
Rayk Oscuro <wer.auch.immer@gmx.net>
2023-12-23 03:51:48 +0100

    Solved day 8 part 1, almost maybe close to solving part 2 as well

---
+++ day8_part1.js
@@ -0,0 +1,26 @@
+import { readFileSync } from "fs";
+import { posix } from "path";
+
+const file = readFileSync("./inputDataDay8.txt", "utf-8");
+const splitFile = file.split("\r\n");
+const directions = splitFile[0];
+const mapStart = [];
+const mapDest = [];
+let currentLoc = "AAA";
+let currentDir = 0;
+let steps = 0;
+for (let x = 2; x < splitFile.length; x++) {
+  mapStart.push(splitFile[x].substring(0,3))
+  mapDest.push([splitFile[x].substring(7,10),splitFile[x].substring(12,15)]);
+}
+//console.log(directions, mapStart, mapDest);
+
+while (currentLoc !== "ZZZ"){
+  steps++;
+  const locIndex= mapStart.indexOf(currentLoc);
+  currentLoc = directions[currentDir]==="L"?mapDest[locIndex][0]:mapDest[locIndex][1];
+  currentDir!==directions.length-1?currentDir++:currentDir=0;
+}
+
+console.log("steps:",steps)
+
---
+++ day8_part2.js
@@ -0,0 +1,74 @@
+import { readFileSync } from "fs";
+import { posix } from "path";
+
+const file = readFileSync("./inputDataDay8.txt", "utf-8");
+const splitFile = file.split("\r\n");
+const directions = splitFile[0];
+const mapStart = [];
+const mapDest = [];
+let currentLoc = [];
+let currentDir = 0;
+let steps = 0;
+const multis = [];
+for (let x = 2; x < splitFile.length; x++) {
+  mapStart.push(splitFile[x].substring(0, 3));
+  mapDest.push([splitFile[x].substring(7, 10), splitFile[x].substring(12, 15)]);
+}
+//console.log(directions, mapStart, mapDest);
+
+for (const loc of mapStart) {
+  if (loc[2] === "A") {
+    currentLoc.push(loc);
+    //    currentLoc.push("ZZZ");
+  }
+}
+
+function escaped(cLocs) {
+  for (const x of cLocs) {
+    if (x[2] !== "Z") {
+      return false;
+    }
+  }
+  return true;
+}
+
+for (let x = 0; x < currentLoc.length; x++) {
+  steps = 0;
+  currentDir = 0;
+  while (currentLoc[x][2] !== "Z") {
+    steps++;
+    const locIndex = mapStart.indexOf(currentLoc[x]);
+    currentLoc[x] =
+      directions[currentDir] === "L"
+        ? mapDest[locIndex][0]
+        : mapDest[locIndex][1];
+    currentDir !== directions.length - 1 ? currentDir++ : (currentDir = 0);
+    if (currentLoc[x][2] === "Z") {
+      console.log("starter:", x, "z at:", steps);
+      multis.push(steps);
+    }
+  }
+}
+
+console.log(multis);
+let result = 1;
+for (const x of multis) {
+  result = result * x;
+}
+console.log(result);
+// while (!escaped(currentLoc)) {
+//   steps++;
+//   for (let x = 0; x < currentLoc.length; x++) {
+//     const locIndex = mapStart.indexOf(currentLoc[x]);
+//     currentLoc[x] =
+//       directions[currentDir] === "L"
+//         ? mapDest[locIndex][0]
+//         : mapDest[locIndex][1];
+//     currentDir !== directions.length - 1 ? currentDir++ : (currentDir = 0);
+//   }
+//   if (steps % 1000000 === 0) {
+//     console.log("steps:", steps);
+//     console.log(currentLoc, escaped(currentLoc));
+//   }
+// }
+console.log("steps:", steps);
