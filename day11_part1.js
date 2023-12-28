a72e50e8a8ed565e1c2e6756aaf767a4a22c8e0c
Rayk Oscuro <wer.auch.immer@gmx.net>
2023-12-28 04:03:03 +0100

    Day 11 was fun!

---
+++ day11_part1.js
@@ -0,0 +1,60 @@
+import { readFileSync } from "fs";
+import { get } from "http";
+import { posix } from "path";
+
+const file = readFileSync("./inputDataDay11.txt", "utf-8");
+const universe = file.split("\r\n").map((x) => x.split(""));
+const expandedV = [];
+const transposedU = [];
+const expandedH = [];
+
+for (let y = 0; y < universe[0].length; y++) {
+  transposedU.push([]);
+}
+
+for (let x = 0; x < universe.length; x++) {
+  const a = new Set();
+  universe[x].map((arrayelements) => a.add(arrayelements));
+  if ([...a].length === 1) {
+    expandedV.push(universe[x]);
+  }
+  expandedV.push(universe[x]);
+}
+
+for (let x = 0; x < expandedV.length; x++) {
+  for (let y = 0; y < expandedV[x].length; y++) {
+    transposedU[y].push(expandedV[x][y]);
+  }
+}
+
+for (let x = 0; x < transposedU.length; x++) {
+  const a = new Set();
+  transposedU[x].map((arrayelements) => a.add(arrayelements));
+  if ([...a].length === 1) {
+    expandedH.push(transposedU[x]);
+  }
+  expandedH.push(transposedU[x]);
+}
+
+//console.log(expandedH);
+
+const galaxyLog = [];
+
+for (let x = 0; x < expandedH.length; x++) {
+  for (let y = 0; y < expandedH[x].length; y++) {
+    if (expandedH[x][y] === "#") {
+      galaxyLog.push([x, y]);
+    }
+  }
+}
+
+console.log(galaxyLog);
+
+let score = 0;
+
+while (galaxyLog.length > 0) {
+  let currentG = galaxyLog.pop();
+  for (let galaxy of galaxyLog){
+    score += Math.abs(currentG[0]-galaxy[0])+Math.abs(currentG[1]-galaxy[1])
+  }
+}
---
+++ day11_part2.js
@@ -0,0 +1,77 @@
+import { readFileSync } from "fs";
+import { get } from "http";
+import { posix } from "path";
+
+const file = readFileSync("./inputDataDay11.txt", "utf-8");
+const universe = file.split("\r\n").map((x) => x.split(""));
+const transposedU = [];
+const hBarriers = [];
+const vBarriers = [];
+
+for (let y = 0; y < universe[0].length; y++) {
+  transposedU.push([]);
+}
+
+for (let x = 0; x < universe.length; x++) {
+  const a = new Set();
+  universe[x].map((arrayelements) => a.add(arrayelements));
+  if ([...a].length === 1) {
+    hBarriers.push(x);
+  }
+}
+
+for (let x = 0; x < universe.length; x++) {
+  for (let y = 0; y < universe[x].length; y++) {
+    transposedU[y].push(universe[x][y]);
+  }
+}
+
+for (let x = 0; x < transposedU.length; x++) {
+  const a = new Set();
+  transposedU[x].map((arrayelements) => a.add(arrayelements));
+  if ([...a].length === 1) {
+    vBarriers.push(x);
+  }
+}
+
+console.log(hBarriers, vBarriers);
+
+const galaxyLog = [];
+
+for (let x = 0; x < universe.length; x++) {
+  for (let y = 0; y < universe[x].length; y++) {
+    if (universe[x][y] === "#") {
+      galaxyLog.push([x, y]);
+    }
+  }
+}
+
+console.log(galaxyLog);
+
+let score = 0;
+
+while (galaxyLog.length > 0) {
+  let currentG = galaxyLog.pop();
+  for (let galaxy of galaxyLog) {
+    score +=
+      Math.abs(currentG[0] - galaxy[0]) + Math.abs(currentG[1] - galaxy[1]);
+    for (let barrier of hBarriers) {
+      if (barrier > currentG[0] && barrier < galaxy[0]) {
+        score += 999999;
+      }
+      if (barrier < currentG[0] && barrier > galaxy[0]) {
+        score += 999999;
+      }
+    }
+    for (let barrier of vBarriers) {
+      if (barrier > currentG[1] && barrier < galaxy[1]) {
+        score += 999999;
+      }
+      if (barrier < currentG[1] && barrier > galaxy[1]) {
+        score += 999999;
+      }
+    }
+  }
+}
+
+console.log(score);
