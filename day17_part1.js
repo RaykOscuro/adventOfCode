34ab776f3d833299682b41c1241e7acb2fd3683c
Rayk Oscuro <wer.auch.immer@gmx.net>
2024-01-09 07:26:50 +0100

    day 17 part 1 solved! but very chaotic. part 2 sadly not getting the right results yet. :(

--- day17_part1.js
+++ day17_part1.js
@@ -16,12 +16,19 @@ const L = 1;
 const D = 2;
 const U = 3;
 
+const rightTurn = [D, U, L, R];
+const leftTurn = [U, D, R, L];
+
 const visitData = [];
 
 for (let row = 0; row < gridData.length; row++) {
   visitData.push([]);
   for (let column = 0; column < gridData[row].length; column++) {
-    visitData[row].push([[], [], []]);
+    visitData[row].push([
+      [Infinity, Infinity, Infinity, Infinity],
+      [Infinity, Infinity, Infinity, Infinity],
+      [Infinity, Infinity, Infinity, Infinity],
+    ]);
   }
 }
 
@@ -34,9 +41,12 @@ const currPos = [
   [1, 0],
 ];
 const currLoss = [gridData[0][1], gridData[1][0]];
-visitData[0][1][0].push(R);
-visitData[1][0][0].push(D);
-//visitData[0][2][1].push(R);
+visitData[0][1][0][R]=gridData[0][1];
+visitData[1][0][0][D]=gridData[1][0];
+
+const finalCoord = [gridData.length - 1, gridData[0].length - 1];
+
+let bestLoss = Infinity;
 
 function isOOB(pos) {
   if (
@@ -57,19 +67,98 @@ while (currPos.length > 0) {
   const pos = currPos.pop();
   const loss = currLoss.pop();
   const onward = [pos[0] + directions[dir][0], pos[1] + directions[dir][1]];
-  // const rightTurn = ...
-  // const leftTurn = ...
+  const right = [
+    pos[0] + directions[rightTurn[dir]][0],
+    pos[1] + directions[rightTurn[dir]][1],
+  ];
+  const left = [
+    pos[0] + directions[leftTurn[dir]][0],
+    pos[1] + directions[leftTurn[dir]][1],
+  ];
+  //  console.log(onward,right,left);
   if (step < 3 && !isOOB(onward)) {
-    if (!visitData[onward[0]][onward[1]][step].includes(dir)) {
-      currStep.push(step + 1);
-      currDir.push(dir);
-      currPos.push(onward);
-      currLoss.push(loss + gridData[onward[0]][onward[1]]);
-      visitData[onward[0]][onward[1]][step] = 1;
+    if (loss + gridData[onward[0]][onward[1]] < bestLoss) {
+      if (
+        !(
+          visitData[onward[0]][onward[1]][step][dir] <=
+          loss + gridData[onward[0]][onward[1]]
+        )
+      ) {
+        if (onward[0] === finalCoord[0] && onward[1] === finalCoord[1]) {
+          console.log(loss + gridData[onward[0]][onward[1]]);
+          if (loss + gridData[onward[0]][onward[1]] < bestLoss) {
+            bestLoss = loss + gridData[onward[0]][onward[1]];
+            console.log(bestLoss);
+          }
+        } else {
+          currStep.push(step + 1);
+          currDir.push(dir);
+          currPos.push(onward);
+          currLoss.push(loss + gridData[onward[0]][onward[1]]);
+          for (let x = step; x < 3; x++) {
+            visitData[onward[0]][onward[1]][x][dir] =
+              loss + gridData[onward[0]][onward[1]];
+          }
+        }
+      }
     }
   }
-  // if (!isOOB(leftTurn)){if (!visitData[rightTurn[0]][rightTurn[1]][0].includes(~dirTurnedLeft)) {...}}
-  // if (!isOOB(rightTurn)){...}
-  // check if final pos reached -> if so, compare heatLoss to current best, update
-  console.log(currPos);
+  if (!isOOB(right)) {
+    if (loss + gridData[right[0]][right[1]] < bestLoss) {
+      if (
+        !(
+          visitData[right[0]][right[1]][0][rightTurn[dir]] <=
+          loss + gridData[right[0]][right[1]]
+        )
+      ) {
+        if (right[0] === finalCoord[0] && right[1] === finalCoord[1]) {
+          console.log(loss + gridData[right[0]][right[1]]);
+          if (loss + gridData[right[0]][right[1]] < bestLoss) {
+            bestLoss = loss + gridData[right[0]][right[1]];
+            console.log(bestLoss);
+          }
+        } else {
+          currStep.push(1);
+          currDir.push(rightTurn[dir]);
+          currPos.push(right);
+          currLoss.push(loss + gridData[right[0]][right[1]]);
+          for (let x = 0; x < 3; x++) {
+            visitData[right[0]][right[1]][x][rightTurn[dir]] =
+              loss + gridData[right[0]][right[1]];
+          }
+        }
+      }
+    }
+  }
+  if (!isOOB(left)) {
+    if (loss + gridData[left[0]][left[1]] < bestLoss) {
+      if (
+        !(
+          visitData[left[0]][left[1]][0][leftTurn[dir]] <=
+          loss + gridData[left[0]][left[1]]
+        )
+      ) {
+        if (left[0] === finalCoord[0] && left[1] === finalCoord[1]) {
+          console.log(loss + gridData[left[0]][left[1]]);
+          if (loss + gridData[left[0]][left[1]] < bestLoss) {
+            bestLoss = loss + gridData[left[0]][left[1]];
+            console.log(bestLoss);
+          }
+        } else {
+          currStep.push(1);
+          currDir.push(leftTurn[dir]);
+          currPos.push(left);
+          currLoss.push(loss + gridData[left[0]][left[1]]);
+          for (let x = 0; x < 3; x++) {
+            visitData[left[0]][left[1]][x][leftTurn[dir]] =
+              loss + gridData[left[0]][left[1]];
+          }
+        }
+      }
+    }
+  }
+  //console.log(currPos);
 }
+
+console.log(visitData);
+console.log(bestLoss);
---
+++ day17_part2.js
@@ -0,0 +1,198 @@
+import { on } from "events";
+import { readFileSync } from "fs";
+import { inflate } from "zlib";
+
+const file = readFileSync("./inputDataDay17.txt", "utf-8");
+const gridData = file.split("\r\n").map((x) => [...x].map((y) => Number(y)));
+
+const directions = [
+  [0, 1],
+  [0, -1],
+  [1, 0],
+  [-1, 0],
+];
+
+const R = 0;
+const L = 1;
+const D = 2;
+const U = 3;
+
+const rightTurn = [D, U, L, R];
+const leftTurn = [U, D, R, L];
+
+const visitData = [];
+
+for (let row = 0; row < gridData.length; row++) {
+  visitData.push([]);
+  for (let column = 0; column < gridData[row].length; column++) {
+    visitData[row].push([
+      [Infinity, Infinity, Infinity, Infinity],
+      [Infinity, Infinity, Infinity, Infinity],
+      [Infinity, Infinity, Infinity, Infinity],
+      [Infinity, Infinity, Infinity, Infinity],
+      [Infinity, Infinity, Infinity, Infinity],
+      [Infinity, Infinity, Infinity, Infinity],
+      [Infinity, Infinity, Infinity, Infinity],
+    ]);
+  }
+}
+
+//console.log(visitData);
+
+const currDir = [R, D];
+const currStep = [4, 4];
+const currPos = [
+  [0, 1],
+  [1, 0],
+];
+const currLoss = [gridData[0][4], gridData[4][0]];
+visitData[0][4][0][R] =
+  gridData[0][1] + gridData[0][2] + gridData[0][3] + gridData[0][4];
+visitData[4][0][0][D] =
+  gridData[1][0] + gridData[2][0] + gridData[3][0] + gridData[4][0];
+
+console.log(visitData[0][4][0]);
+console.log(visitData[4][0][0]);
+const finalCoord = [gridData.length - 1, gridData[0].length - 1];
+
+let bestLoss = Infinity;
+
+function isOOB(pos) {
+  if (
+    !(pos[0] >= gridData.length) &&
+    !(pos[1] >= gridData[0].length) &&
+    !(pos[0] < 0) &&
+    !(pos[1] < 0)
+  ) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+while (currPos.length > 0) {
+  const step = currStep.pop();
+  const dir = currDir.pop();
+  const pos = currPos.pop();
+  const loss = currLoss.pop();
+  let onward = [-1, -1];
+  if (step < 10) {
+    onward = [pos[0] + directions[dir][0], pos[1] + directions[dir][1]];
+  }
+  const right = [
+    pos[0] + 4 * directions[rightTurn[dir]][0],
+    pos[1] + 4 * directions[rightTurn[dir]][1],
+  ];
+  const left = [
+    pos[0] + 4 * directions[leftTurn[dir]][0],
+    pos[1] + 4 * directions[leftTurn[dir]][1],
+  ];
+  let rightLoss = Infinity;
+  if (!isOOB(right)) {
+    rightLoss =
+      loss +
+      gridData[pos[0] + directions[rightTurn[dir]][0]][
+        pos[1] + directions[rightTurn[dir]][1]
+      ] +
+      gridData[pos[0] + 2 * directions[rightTurn[dir]][0]][
+        pos[1] + 2 * directions[rightTurn[dir]][1]
+      ] +
+      gridData[pos[0] + 3 * directions[rightTurn[dir]][0]][
+        pos[1] + 3 * directions[rightTurn[dir]][1]
+      ] +
+      gridData[pos[0] + 4 * directions[rightTurn[dir]][0]][
+        pos[1] + 4 * directions[rightTurn[dir]][1]
+      ];
+  }
+  let leftLoss = Infinity;
+  if (!isOOB(left)) {
+    leftLoss =
+      loss +
+      gridData[pos[0] + directions[leftTurn[dir]][0]][
+        pos[1] + directions[leftTurn[dir]][1]
+      ] +
+      gridData[pos[0] + 2 * directions[leftTurn[dir]][0]][
+        pos[1] + 2 * directions[leftTurn[dir]][1]
+      ] +
+      gridData[pos[0] + 3 * directions[leftTurn[dir]][0]][
+        pos[1] + 3 * directions[leftTurn[dir]][1]
+      ] +
+      gridData[pos[0] + 4 * directions[leftTurn[dir]][0]][
+        pos[1] + 4 * directions[leftTurn[dir]][1]
+      ];
+  }
+  //console.log(onward,right,left);
+  if (!isOOB(onward)) {
+    if (loss + gridData[onward[0]][onward[1]] < bestLoss) {
+      if (
+        !(
+          visitData[onward[0]][onward[1]][step - 3][dir] <=
+          loss + gridData[onward[0]][onward[1]]
+        )
+      ) {
+        if (onward[0] === finalCoord[0] && onward[1] === finalCoord[1]) {
+          //console.log(loss + gridData[onward[0]][onward[1]]);
+          if (loss + gridData[onward[0]][onward[1]] < bestLoss) {
+            bestLoss = loss + gridData[onward[0]][onward[1]];
+            console.log(bestLoss);
+          }
+        } else {
+          currStep.push(step + 1);
+          currDir.push(dir);
+          currPos.push(onward);
+          currLoss.push(loss + gridData[onward[0]][onward[1]]);
+          for (let x = step - 3; x < 7; x++) {
+            visitData[onward[0]][onward[1]][x][dir] =
+              loss + gridData[onward[0]][onward[1]];
+          }
+        }
+      }
+    }
+  }
+  if (!isOOB(right)) {
+    if (rightLoss < bestLoss) {
+      if (!(visitData[right[0]][right[1]][0][rightTurn[dir]] <= rightLoss)) {
+        if (right[0] === finalCoord[0] && right[1] === finalCoord[1]) {
+          //console.log(loss + gridData[right[0]][right[1]]);
+          if (rightLoss < bestLoss) {
+            bestLoss = rightLoss;
+            console.log(bestLoss);
+          }
+        } else {
+          currStep.push(4);
+          currDir.push(rightTurn[dir]);
+          currPos.push(right);
+          currLoss.push(rightLoss);
+          for (let x = 0; x < 7; x++) {
+            visitData[right[0]][right[1]][x][rightTurn[dir]] = rightLoss;
+          }
+        }
+      }
+    }
+  }
+  if (!isOOB(left)) {
+    if (leftLoss < bestLoss) {
+      if (!(visitData[left[0]][left[1]][0][leftTurn[dir]] <= leftLoss)) {
+        if (left[0] === finalCoord[0] && left[1] === finalCoord[1]) {
+          //console.log(loss + gridData[left[0]][left[1]]);
+          if (leftLoss < bestLoss) {
+            bestLoss = leftLoss;
+            console.log(bestLoss);
+          }
+        } else {
+          currStep.push(4);
+          currDir.push(leftTurn[dir]);
+          currPos.push(left);
+          currLoss.push(leftLoss);
+          for (let x = 0; x < 7; x++) {
+            visitData[left[0]][left[1]][x][leftTurn[dir]] = leftLoss;
+          }
+        }
+      }
+    }
+  }
+  //console.log(currPos);
+}
+
+//console.log(visitData);
+console.log(bestLoss);
